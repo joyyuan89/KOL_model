6b237068b3159f779bbcb72dac7e3629d8e6377f
Jake-wen-github <to.wenjingzhou@gmail.com>
2023-02-11 22:44:35 +0800

    2023-02-11 PM

--- OpenAI embedding.py
+++ OpenAI embedding.py
@@ -19,9 +19,12 @@ work_dir = os.getcwd()
 input_path = "/Users/jakewen/Desktop/Github/KOL_model/INPUT/central_bank_speech/all_speeches.csv"
 
 speeches_data = pd.read_csv(input_path)
+speeches_data['date'] = pd.to_datetime(speeches_data['date'], format="%d/%m/%Y")
+speeches_data.set_index('date', inplace=True)
 
 # randomly select 10 row for test
-df_sample = speeches_data.sample(n = 5)
+# df_sample = speeches_data.sample(n = 3)
+df_sample = speeches_data.tail(200)
 
 #%% Vectorizer
 from keyphrase_vectorizers import KeyphraseCountVectorizer
@@ -65,21 +68,46 @@ def cosine_similarity_function(vec_1, vec_2):
     return value[0][0]
 
 #%% Variables
-
-Search_word = 'inflation'
+search_word = 'recession'
 
 #%% Main body
-
 import time
 start_time = time.time()
+print("program running")
 
-Search_word_embedding = keyBERT_embedding(Search_word)
+Search_word_embedding = keyBERT_embedding(search_word)
 
 df_keywords = df_sample.copy()
 
-df_keywords["keywords"] = df_keywords["text"].apply(lambda x: keyBERT_keyword(x ,10))
+# df_keywords["keywords"] = df_keywords["text"].apply(lambda x: keyBERT_keyword(x ,10))
 df_keywords["text_embedding"] = df_keywords["text"].apply(lambda x: keyBERT_embedding(x))
-df_keywords["keywords_embedding"] = df_keywords["keywords"].apply(lambda x: keyBERT_embedding(x))
-df_keywords["Cosine_similarity_article"] = df_keywords["text_embedding"].apply(lambda x: cosine_similarity_function(x, Search_word_embedding))
 
+# df_keywords["keywords_embedding"] = df_keywords["keywords"].apply(lambda x: keyBERT_embedding(x))
+df_keywords[search_word] = df_keywords["text_embedding"].apply(lambda x: cosine_similarity_function(x, Search_word_embedding))
+
+print("program completed")
 print("--- %s seconds ---" % (time.time() - start_time))
+
+#%% Post processing
+
+date_range = pd.date_range(start=df_keywords.index.min(), end=df_keywords.index.max(),freq='D')
+date_range = date_range.to_frame()
+df_output = pd.merge(df_keywords, date_range, left_index=True, right_index=True, how='outer')
+df_output.fillna(0,inplace=True)
+df_output["value"] = df_output[search_word].rolling(window=30, min_periods=1).sum()
+
+#%% Plot
+import matplotlib.pyplot as plt
+
+x = df_output[0]
+y = df_output["value"]
+
+plt.xlabel("Date")
+plt.ylabel(search_word+" value")
+plt.title(search_word+" trend")
+
+plt.plot(x, y)
+plt.show()
+
+#%% Download
+df_output.to_excel("df_output.xlsx")
