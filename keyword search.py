10cbb77d1e39242a5db75c22d7ebfcd02fa8e249
Jake-wen-github <to.wenjingzhou@gmail.com>
2023-02-12 10:22:15 +0800

    2023-02-12

--- keyword search.py
+++ keyword search.py
@@ -53,25 +53,42 @@ def cosine_similarity_function(vec_1, vec_2):
 
 #%% Variables
 search_word = 'recession'
+effective_date_list = [
+    [30,1],     # within next 30 day, full impact
+    [90,0.25],   # within next 60 day, half impact
+    [180,0.1],  # within next 180 day, quarter impact
+    ]
+min_threshold = 0.15
+power = 6
 
 #%% Main body
-print("program running")
-
 Search_word_embedding = embedding(search_word)
-
 df_keywords = df_sample.copy()
-
 df_keywords[search_word] = df_keywords["text_embedding"].apply(lambda x: cosine_similarity_function(x, Search_word_embedding))
 
-print("program completed")
+df_keywords_copy = df_keywords.copy()
 
 #%% Post processing
+# adjust similarity value
+def adjust_value(value):
+    if value < min_threshold:
+        value = 0
+    value = value**power
+    
+    return value
 
+df_keywords[search_word] = df_keywords[search_word].apply(lambda x: adjust_value(x))
+
+
+# fill the date gaps
 date_range = pd.date_range(start=df_keywords.index.min(), end=df_keywords.index.max())
 date_range = date_range.to_frame()
 df_output = pd.merge(df_keywords, date_range, left_index=True, right_index=True, how='outer')
-df_output.fillna(0,inplace=True)
-df_output["value"] = df_output[search_word].rolling(window=180, min_periods=1).sum()
+
+# create value with time decay
+df_output["value"] = 0
+for i in effective_date_list:
+    df_output["value"] += df_output[search_word].rolling(window=i[0], min_periods=1).sum()*i[1]
 
 #%% Plot
 import matplotlib.pyplot as plt
@@ -85,3 +102,20 @@ plt.title(search_word+" trend")
 
 plt.plot(x, y)
 plt.show()
+
+#%% Plotly
+import plotly.express as px
+import plotly.io as pio
+pio.renderers.default = 'browser'
+#pio.renderers.default = 'svg'
+
+x = df_output.index
+y = df_output["value"]
+
+fig = px.line(
+    df_output, 
+    x=df_output.index, 
+    y = df_output["value"],
+    title=search_word+" trend")
+
+fig.show()
