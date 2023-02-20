5058074c93cab2eb68dbed014f698df5712702df
Jiayue <joyyuan89@gmail.com>
2023-02-20 14:40:06 +0800

    add keyword extraction app

---
+++ app/Home.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb 20 09:55:07 2023
+
+@author: jiayue.yuan
+"""
+
+import streamlit as st
+
+st.markdown("# Home Page 🎈")
+
+st.sidebar.markdown("# Home Page 🎈")
+st.sidebar.markdown("1.introduction")
+st.sidebar.markdown("2.keyword serch")
+st.sidebar.markdown("3.topic trend")
+st.sidebar.markdown("4.topic treemap")
